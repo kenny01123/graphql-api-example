bd08bc33b51985e43572d3df9ddaf798060dd442
Wai Cheong Ng Kenny <kenny01123@Wais-MacBook-Pro.local>
2020-10-14 12:02:35 +0900

    IMplemented schema
    Co-authored-by: Jaymontojo673 <jaymontojo673@users.noreply.github.com>
    Co-authored-by: kenny01123 <kenny01123@users.noreply.github.com>

--- server/schema.js
+++ server/schema.js
@@ -5,9 +5,30 @@ const typeDefs = gql`
   # The schema should model the full data object available!
 
   type Pokemon {
-    id: String
+    id: String!
     name: String!
+    classification: String
+    types: [String]
+    height: Height
+    weight: Weight
+    evolutions: [Evolutions]
   }
+
+  type Height {
+    minimum: String
+    maximum: String
+  }
+
+  type Weight {
+    minimum: String
+    maximum: String
+  }
+
+  type Evolutions {
+    id: Int!
+    name: String
+  }
+
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
